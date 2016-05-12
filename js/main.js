5962f799abaf773c39f66a52144d7a4e8cbc2226
Charles6 <charles@devleague.com>
2016-05-11 21:38:26 -1000

    Added scoring and lose condition

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var spaceKey;
+var score = -1;
 
 var mainState = {
 
@@ -13,19 +15,68 @@ var mainState = {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.stage.backgroundColor = '#3498db';
 		game.renderer.renderSession.roundPixels = true;
-		game.load.image('player', 'assets/player.png');
-		this.player = game.add.sprite(game.width/8, game.height*(7/8), 'player');
-		game.physics.arcade.enable(this.player);
-		this.player.body.gravity.y = 500;
+
 		platforms = game.add.group();
     	platforms.enableBody = true;
-		var ground = platforms.create(0, game.world.height*(11/12), 'ground');
-    	ground.scale.setTo(4, 1);
-			game.physics.arcade.enable(platforms);
-			game.physics.arcade.collide(this.player, platforms);
-    	ground.body.immovable = true;
+
+			this.ground = platforms.create(0, game.world.height, 'ground');
+			this.ground.anchor.setTo(0,1);
+    	this.ground.scale.setTo(4, 1);
+			game.physics.arcade.enable(this.ground);
+    	this.ground.body.immovable = true;
+
+
+			this.player = game.add.sprite(game.width/8, game.world.height*(7/8), 'player');
+			game.physics.arcade.enable(this.player);
+			this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+			this.player.body.bounce.y = 0.2;
+	    this.player.body.gravity.y = 300;
+	    // this.player.body.this.ground = true;
+
+			this.wall = game.add.sprite(700,game.world.height, 'obsticle');
+			this.wall.scale.setTo(1,0.2);
+			this.wall.anchor.setTo(0,1);
+			game.physics.arcade.enable(this.wall);
+			this.wall.body.immovable = true;
+
+
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+
 	},
 	update: function(){
+		game.physics.arcade.collide(this.player, this.ground);
+		game.physics.arcade.collide(this.player, this.wall);
+    this.player.body.velocity.x = 0;
+		if (this.wall.x < 0) {
+				console.log("new wall");
+				this.wall.kill();
+				this.wall = game.add.sprite(900,game.world.height, 'obsticle');
+				this.wall.scale.setTo(1,0.2);
+				this.wall.anchor.setTo(0,1);
+				game.physics.arcade.enable(this.wall);
+				this.wall.body.immovable = true;
+		} else if (this.wall.x > 600) {
+			this.wall.x -= 0.05;
+		};
+
+		if (this.spaceKey.isDown&&this.player.body.touching.down){
+        this.player.body.velocity.y = -300;
+    };
+
+		if (this.wall.x < 5 && this.player.x > 5){
+			score++;
+			scoreText.text = 'score: ' + score;
+		}
+
+		if (this.player.x < 0){
+			scoreText = game.add.text(350,200, 'You Lose!', { fontSize: '100px', fill: '#ff0000' });
+			this.wall.kill();
+		}
+
+
+
 
 	}
 };
