2fe8690e3d880ba47c9ba4a7ffd7b77114fb8aa7
Charles Nguyen <charles@devleague.com>
2016-05-13 15:39:56 -1000

    clean up for class
    Removed the app.js document and cleaned up main.js to include clear
    comments on how each component works.

--- js/main.js
+++ js/main.js
@@ -1,88 +1,111 @@
+//This sets the variable for the spacebar.
 var spaceKey;
+
+//This sets the score to start at -1.
 var score = -1;
 
 var mainState = {
 
 	preload: function(){
+
+		//These foure things sets the assets for the game. If you want to add music or images, there is where you would preload it.
 		game.load.image('background', 'assets/background.png');
 		game.load.image('player', 'assets/player.png');
 		game.load.image('ground', 'assets/wallHorizontal.png');
 		game.load.image('obsticle', 'assets/wallVertical.png');
 	},
 
-
 	create: function(){
+
+		//This sets the game physics to Arcade style.
 		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		//This sets the background color to #3498db on the hex system.
 		game.stage.backgroundColor = '#3498db';
+
+		//This gives us sharp corners for all of our images.
 		game.renderer.renderSession.roundPixels = true;
 
+		//This sets up a group call platforms. For future functionality we can set all horizontal surfaces to this group.
 		platforms = game.add.group();
     	platforms.enableBody = true;
 
-			this.ground = platforms.create(0, game.world.height, 'ground');
-			this.ground.anchor.setTo(0,1);
+    	//This creates the ground, and makes it solid object the player will not pass through.
+		this.ground = platforms.create(0, game.world.height, 'ground');
+		this.ground.anchor.setTo(0,1);
     	this.ground.scale.setTo(4, 1);
-			game.physics.arcade.enable(this.ground);
+		game.physics.arcade.enable(this.ground);
     	this.ground.body.immovable = true;
 
+    	//This creates the player character at the bottom left side of the screen.
+		this.player = game.add.sprite(game.width/8, game.world.height*(7/8), 'player');
+		game.physics.arcade.enable(this.player);
 
-			this.player = game.add.sprite(game.width/8, game.world.height*(7/8), 'player');
-			game.physics.arcade.enable(this.player);
-			this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		//This sets the spacebar key as the input for this game.
+		this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-			this.player.body.bounce.y = 0.2;
-	    this.player.body.gravity.y = 300;
-	    // this.player.body.this.ground = true;
+		//This sets the physics on the player in terms of the gravity and the bounce.
+		this.player.body.bounce.y = 0.2;
+	    this.player.body.gravity.y = 600;
 
-			this.wall = game.add.sprite(700,game.world.height, 'obsticle');
-			this.wall.scale.setTo(1,0.2);
-			this.wall.anchor.setTo(0,1);
-			game.physics.arcade.enable(this.wall);
-			this.wall.body.immovable = true;
+	    //This creates the first obsticle on the right side of the screen.
+		this.obsticle = game.add.sprite(700,game.world.height, 'obsticle');
+		this.obsticle.scale.setTo(1,0.2);
+		this.obsticle.anchor.setTo(0,1);
+		game.physics.arcade.enable(this.obsticle);
+		this.obsticle.body.immovable = true;
 
-
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-
+		//This adds the scoreboard on the top left side of the screen.
+    	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 	},
+
 	update: function(){
+
+		//This is where the game engine recognizes collision betwen the player and the ground or the obsticle.
 		game.physics.arcade.collide(this.player, this.ground);
-		game.physics.arcade.collide(this.player, this.wall);
-    this.player.body.velocity.x = 0;
-		if (this.wall.x < 0) {
-				console.log("new wall");
-				this.wall.kill();
-				this.wall = game.add.sprite(900,game.world.height, 'obsticle');
-				this.wall.scale.setTo(1,0.2);
-				this.wall.anchor.setTo(0,1);
-				game.physics.arcade.enable(this.wall);
-				this.wall.body.immovable = true;
-		} else if (this.wall.x > 600) {
-			this.wall.x -= 0.05;
+		game.physics.arcade.collide(this.player, this.obsticle);
+
+		//This will set the horizontal movement to zero.
+    	this.player.body.velocity.x = 0;
+
+    	//This will create a new wall if the old wall goes off the screen.
+		if (this.obsticle.x < 0) {
+			this.obsticle.kill();
+			this.obsticle = game.add.sprite(900,game.world.height, 'obsticle');
+			this.obsticle.scale.setTo(1,0.2);
+			this.obsticle.anchor.setTo(0,1);
+			game.physics.arcade.enable(this.obsticle);
+			this.obsticle.body.immovable = true;
 		};
 
-		if (this.spaceKey.isDown&&this.player.body.touching.down){
-        this.player.body.velocity.y = -300;
-    };
+		//This will move the obsticle to the left if it is on the right side of the screen.
+		if (this.obsticle.x > 600) {
+			this.obsticle.x -= 0.05;
+		};
 
-		if (this.wall.x < 5 && this.player.x > 5){
+		//This allows the player to jump only if you press the space key and the player is touching the something at the bottom.
+		if (this.spaceKey.isDown && this.player.body.touching.down){
+        	this.player.body.velocity.y = -300;
+    	};
+
+    	//This will update the score if the player has not been pushed off the screen, and the wall has gone off the left side.
+		if (this.obsticle.x < 5 && this.player.x > 5){
 			score++;
 			scoreText.text = 'score: ' + score;
-		}
+		};
 
+		//This will tell you "You Lose!" if the player is pushed off the left side of the screen.
 		if (this.player.x < 0){
-			scoreText = game.add.text(350,200, 'You Lose!', { fontSize: '100px', fill: '#ff0000' });
-			this.wall.kill();
-		}
-
-
-
-
+			scoreText = game.add.text(350,200, 'You Lose!', {fill: '#ff0000'});
+			this.obsticle.kill();
+			this.player.kill();
+		};
 	}
 };
 
-
+//This sets the size of the game screen and sets it to the div "gameDiv".
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
 
+//This starts the game.
 game.state.add('main', mainState);
 game.state.start('main');
